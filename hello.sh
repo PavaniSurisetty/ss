eeedc92954d4a51615e3eacca58255f6cbffa7e0
PAVANI S <pavsurisetty@gmail.com>
2024-04-20 09:32:52 +0530

    adding

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "hello world i am learning shell script"
+echo "Ramesh : hi "
+echo "Suresh : hello Gd mrng "
+echo "Ramesh : very good mrng "
+echo "Suresh : where r u going"
+echo "Ramesh : i am going to office"
+echo "Suresh : what about you"
+echo "Ramesh : ya good "
