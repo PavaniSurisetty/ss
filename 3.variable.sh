58f910715cee0656535749f877eb52c95f46268e
PAVANI S <pavsurisetty@gmail.com>
2024-04-20 09:44:28 +0530

    adding new file

---
+++ 3.variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+person1=$1
+person2=$2
+echo "$person1 : hi "
+echo "$person2 : hello Gd mrng "
+echo "$person1 : very good mrng "
+echo "$person2 : where r u going"
+echo "$person1 : i am going to office"
+echo "$person2 : what about you"
+echo "$person1 : ya good "
