7937ad958b6a65451b7cac9a5a7b0bdd527a4044
PAVANI S <pavsurisetty@gmail.com>
2024-04-20 11:38:15 +0530

    date

--- 5.variables.sh
+++ 5.variables.sh
@@ -10,5 +10,11 @@ echo "username Entered is: $username"
 echo "Please enter your password"
 
 read "password"
- 
+
+echo "password Entered is: $username"
+
+echo "password Entered is: $password"
+
+
+
 
