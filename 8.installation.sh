c98123ca4788879f4cb6f29a532e7da14d061449
PAVANI S <pavsurisetty@gmail.com>
2024-04-22 09:51:30 +0530

    Some msg

--- 8.installation.sh
+++ 8.installation.sh
@@ -3,4 +3,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "error : please run with root access "
+exit $?
 fi 
+yum install git -y 
+yum install mysql -y
+ if [ $? -ne 0 ]
+ then "installation is successfull "
