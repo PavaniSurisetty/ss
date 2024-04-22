de357d588297a83d2ab72b5fa9c3deb253a58a59
PAVANI S <pavsurisetty@gmail.com>
2024-04-22 10:02:16 +0530

    Some installations

--- 8.installation.sh
+++ 8.installation.sh
@@ -3,10 +3,26 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "error : please run with root access "
-exit $?
+exit 1
+else "you are a super user"
 fi 
-yum install git -y 
+
 yum install mysql -y
  if [ $? -ne 0 ]
- then "installation is successfull "
+ then 
+    echo "installation of my sql is successfull "
+ exit 1
+ else 
+    echo "installation is failed"
  fi
+
+ yum install git -y 
+
+ if [ $? -ne 0 ]
+ then 
+    echo "installation of git is successfull "
+    exit 1
+ else 
+    echo "installation is failed"
+ fi
+ echo "is scripting proceeding"
