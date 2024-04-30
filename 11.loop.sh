6c4138795f21135586048594e1b6ce03f37c8a99
PAVANI S <pavsurisetty@gmail.com>
2024-04-30 10:11:54 +0530

    adding loops

--- 11.loop.sh
+++ 11.loop.sh
@@ -2,13 +2,18 @@
 USERID=$(id -u)
 SCRIPTNAME=$0
 G="\e[32m"
+DATE=$(date +%F)
 for i in #@
 do 
     yum install $i -y
 done
 
 if [ $USERID -eq 0 ] 
-    then echo -e " $G installing is success"
+    then echo -e " $G  $i installing is success"
+    exit 1
 else
-    then echo " take root access"
+    echo " take root access"
+fi
+if [ $1 -eq 0 ]
+then echo " $DATE "
 fi
