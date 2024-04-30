b62d045eedc6b5d91594613aac0059312535c4f3
PAVANI S <pavsurisetty@gmail.com>
2024-04-30 10:03:01 +0530

    adding loops

--- 11.loop.sh
+++ 11.loop.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+USERID=$(id -u)
+SCRIPTNAME=$0
+G="\e[32m"
+for i in #@
+do 
+    yum install $i -y
+done
+
+if [ $USERID -ne 0 ] {
+    then echo " take root access"
+}
+else 
+    echo -e " $G installing is success"
+fi
